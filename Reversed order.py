36e57534f7d0048a90f93b8bf426814f23ad96aa
Nooria <samihamaysoonnooria@gmail.com>
2025-02-06 15:41:49 +0600

    first commit

---
+++ Reversed order.py
@@ -0,0 +1,4 @@
+print("To got to reverse order in a loop we have to mention another parameter -1")
+
+for i in range(10, 0, -1):
+    print(i)
---
+++ Sum of numbers.py
@@ -0,0 +1,6 @@
+n = int(input("Enter a number: "))
+
+sum = 0
+for i in range(0,n):
+    sum = sum + i
+    print(f"The sum of i={i} iteration : {sum} ")
