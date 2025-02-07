65cd90ca8ac61f5537052225aea6bc826b2a2231
Nooria <samihamaysoonnooria@gmail.com>
2025-02-07 08:24:07 +0600

    bhjj

---
+++ Power Calculator/Power Calculator.py
@@ -0,0 +1,6 @@
+name = str(input("Broski, please enter your name: "))
+base = int(input("Enter the base number: "))
+power = int(input("Enter the power: "))
+
+result = base ** power
+print(f"Yoi {name}, the base {base} to the power {power} is {result}")
